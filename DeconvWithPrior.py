039307eff76c43f74c7eebc2fcca315287749f76
Bene <bene.d@gmx.de>
2017-07-10 21:04:45 +0200

    Updated File.

--- DeconvWithPrior.py
+++ DeconvWithPrior.py
@@ -10,13 +10,17 @@ import SimNoiseImg as simn
 from SimNoiseImg import tf_iFT, tf_FT
 import numpy as np
 import tensorflow as tf
-
+import matplotlib.pyplot as plt
 
 img_noise,img,otf,obj = simn.SimNoiseImg('./devoogd.tif');
 
 
-guess = img_noise*0+np.mean(img_noise)
-mylambda=0.1
+plt.imshow(img_noise)
+plt.show()
+
+
+guess = img_noise*0.0+np.mean(img_noise)
+mylambda=1
 
 #totalError, myDeriv = simn.PoissonErrorAndDeriv(guess,img_noise,otf,mylambda)
 
@@ -29,11 +33,25 @@ tf_mylambda = tf.constant(mylambda, dtype = tf.float32, name = "tf_mylambda")
 
 
 # define error metric
-totalError, myFWD = simn.PoissonErrorAndDerivTF(tf_guess,tf_measured,tf_otf,tf_mylambda)
+#totalError, myFWD = simn.PoissonErrorAndDerivTF(tf_guess,tf_measured,tf_otf,tf_mylambda)
+
+
+NormFac = 1;
+
+# tensorflow's FFT cannot handle non-complex values -> convert! 
+tf_guess_cmplx = tf.complex(tf_guess, tf.zeros(tf_guess.get_shape()));
+tf_otf_cmplx = tf.complex(tf_otf, tf.zeros(tf_guess.get_shape()));
+
+Fwd = tf.real(tf_iFT(tf_FT(tf_guess_cmplx) * tf_otf_cmplx))
+totalError = tf.reduce_sum((Fwd-tf_measured) - tf_measured * tf.log(Fwd/(tf_measured)), reduction_indices=[0, 1])
+totalError = totalError + tf_mylambda*tf.reduce_sum(tf_guess**2, reduction_indices=[0, 1]);
+
+myRes = (1 - tf_measured / Fwd)
+totalError = tf.real(totalError * NormFac)
 
 
 # define optimizer
-optimizer = tf.train.AdamOptimizer(learning_rate=0.1)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
 train_step = optimizer.minimize(totalError)
 
 # Initializing the variables
@@ -45,13 +63,13 @@ NEPOCH = 100
 for i in range(NEPOCH):
     sess.run(train_step)
     
-    print(tf_loss)
+    
     if(np.mod(i, 10)):
         tf_loss = sess.run(totalError)
         result = tf_guess.eval()     
         plt.imshow(result, cmap='gray')
         plt.show()
-
+        print(tf_loss)
        
 # display all the different results       
 # reconstructed result
